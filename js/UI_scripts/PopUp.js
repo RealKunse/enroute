d89c49169c44af2e9647ea3160af605deba0e6cd
RealKunse <gml4009@gmail.com>
2021-02-18 17:28:20 +0900

    0.0.3

---
+++ js/UI_scripts/PopUp.js
@@ -0,0 +1,9 @@
+function popupFirst() {
+    // document.getElementById('visible').style.top = "0";
+    document.getElementById('visible-block').style.bottom = "0";
+}
+
+function resetFirst() {
+    // document.getElementById('visible').style.top = "-100%";
+    document.getElementById('visible-block').style.bottom = "-100%";
+}
